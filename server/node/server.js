7beba1a3c20e1865e2249cf274cc6e0b67d9b41c
Dariusz <dariusz.mendocha12@gmail.com>
2018-05-20 01:50:31 +0200

    Init

---
+++ server/node/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
+app.listen(port, function () {
+    console.info('Listening');
+});
